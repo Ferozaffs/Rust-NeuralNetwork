6f69ccdeb845d0400bf0d5be89f94170665e5912
Ferozaffs <zmass@ZMASS>
2023-06-30 01:05:55 +0200

    working neural net

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
 pub mod neural_nets;
-use nalgebra::DVector;
+use nalgebra::{DVector, dvector};
 
-
+use std::env;
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+
     let mut net = neural_nets::create_neural_net(3,3,5,5);
     net.debug_print();
 
     let mut rng = rand::thread_rng();
     let dist = rand::distributions::Standard;
 
-    let mut previous_avg_cost: f32 = 1.0;
-	for _generation in 0..5 {
+    let num_generations = 500000;
+    let num_tests = 100;
+
+	for _generation in 0..num_generations {
 		let mut cost: f32 = 0.0;
 
-		for _test in 0..1 {
+		for _test in 0..num_tests {
             let input = DVector::from_distribution(3, &dist, &mut rng);
             let outputs = net.feed_forward(&input);
-            println!("INPUT{} OUTPUT{}",&input, &outputs);
+
+            if outputs[0] > outputs[1] && outputs[0] > outputs[2]{
+				cost += net.calculate_cost(&outputs, &dvector![1.0, 0.0, 0.0]);
+			}
+			else if outputs[1] > outputs[0] && outputs[1] > outputs[2]{
+				cost += net.calculate_cost(&outputs, &dvector![0.0, 1.0, 0.0]);
+			}
+			else{
+				cost += net.calculate_cost(&outputs, &dvector![0.0, 0.0, 1.0]);
+			}		
         }
+
+		let learning_rate_max = 0.1;
+		let learning_rate_min = 0.01;
+        let avg_cost = cost / num_tests as f32;
+		net.train(learning_rate_min + avg_cost * (learning_rate_max - learning_rate_min));
+	
+        println!("{} Avg.cost\n", avg_cost);
     }
 }
--- src/neural_nets.rs
+++ src/neural_nets.rs
@@ -8,6 +8,10 @@ pub fn disigmoid(value: f32) -> f32{
 	return value * (1.0 - value);
 }
 
+pub fn to_open(value: f32) -> f32{
+	return value * 2.0 - 1.0;
+}
+
 pub struct Layer{
 	layer_weights: DMatrix<f32>,
 	layer_bias: DVector<f32>,
@@ -19,12 +23,17 @@ pub fn create_layer(num_inputs: usize, num_nodes: usize) -> Layer {
 	let mut rng = rand::thread_rng();
     let dist = rand::distributions::Standard;
 
-	return Layer { 
+	let mut layer = Layer { 
 		layer_weights: DMatrix::from_distribution(num_nodes, num_inputs, &dist, &mut rng), 
 		layer_bias: DVector::from_distribution(num_nodes, &dist, &mut rng),
-		layer_inputs: DVector::identity(num_inputs),
-		layer_outputs: DVector::identity(num_nodes) 
-	}
+		layer_inputs: DVector::zeros(num_inputs),
+		layer_outputs: DVector::zeros(num_nodes) 
+	};
+
+	layer.layer_weights = layer.layer_weights.map(to_open);
+	layer.layer_bias = layer.layer_bias.map(to_open);
+
+	return layer;
 }	
 
 
@@ -33,6 +42,7 @@ pub struct NeuralNet{
 	num_outputs: usize,
 	layers: Vec<(usize, Layer)>,
 	accumelated_cost: f32,
+	accumelated_guesses: DVector<f32>,
 	num_calcs: i32
 }
 
@@ -47,20 +57,74 @@ impl NeuralNet {
 
 	pub fn feed_forward(&mut self, initial_input: &DVector<f32>) -> DVector<f32> {
 		let mut input = initial_input.clone();
-		let mut output = DVector::identity(1);
+		let mut output = DVector::zeros(1);
 		
 		for layer in self.layers.iter_mut(){
-			layer.1.layer_inputs = input.clone();
+			layer.1.layer_inputs += &input;
 			
-			output = &layer.1.layer_weights * input;
-			output = output + &layer.1.layer_bias;
+			output = &layer.1.layer_weights * &input;
+			output += &layer.1.layer_bias;
 			output = output.map(sigmoid);
 
-			layer.1.layer_outputs = output.clone();
+			layer.1.layer_outputs += &output;
 			input = output.clone();
 		}
 
 		return output;
+	}
+
+	pub fn calculate_cost(&mut self, previous_output: &DVector<f32>, correct_output: &DVector<f32>) -> f32{
+		self.num_calcs += 1;
+	
+		
+		let mut cost = 0.0;
+
+		for n in 0..previous_output.data.len() {
+			cost += libm::powf(previous_output[n], 2.0);
+		}
+
+		self.accumelated_cost += cost;
+
+		let delta = previous_output - correct_output;
+		self.accumelated_guesses += delta;
+
+		return cost;
+	}
+
+	pub fn train(&mut self, learning_rate: f32) {
+		let scale = 1.0 / self.num_calcs as f32;
+
+		let avg_targets = &self.accumelated_guesses * scale;
+
+		let avg_outputs = &self.layers[self.layers.len() - 1].1.layer_outputs * scale;
+	
+		let mut errors = (avg_outputs - avg_targets) * 2.0;
+	
+		for n in (0..self.layers.len()).rev(){
+			let layer = &mut self.layers[n].1; 
+
+			let avg_outputs = &layer.layer_outputs * scale;
+			let mapped = avg_outputs.map(disigmoid);
+
+			let mut gradients = DVector::identity(errors.data.len());
+			for x in 0..mapped.data.len() {
+				gradients[x] = mapped[x] * errors[x];
+			}
+
+			let avg_inputs = &layer.layer_inputs * scale;
+			let transposed = avg_inputs.transpose();
+			
+			errors = layer.layer_weights.transpose() * &gradients;
+
+			layer.layer_weights -= &gradients * transposed;
+			layer.layer_bias -= &gradients * learning_rate;
+			layer.layer_inputs *= 0.0;
+			layer.layer_outputs *= 0.0;			
+		}
+
+		self.accumelated_guesses *= 0.0;
+		self.accumelated_cost = 0.0;
+		self.num_calcs = 0;
 	}
 }
 
@@ -70,6 +134,7 @@ pub fn create_neural_net(num_inputs: usize, num_outputs: usize, num_layers: usiz
 		num_outputs: num_outputs,
 		layers: Vec::new(),
 		accumelated_cost: 0.0,
+		accumelated_guesses: DVector::zeros(num_outputs),
 		num_calcs: 0
 
 	};
